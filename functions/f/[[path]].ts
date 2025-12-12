c68fe703b0c58d49e74fa233ecd95d2dc4ee5628
shauryadotp <shauryadotp@outlook.jp>
2025-12-12 15:16:33 +0530

    Refactor image URL construction and fetch logic

--- functions/f/[[path]].ts
+++ functions/f/[[path]].ts
@@ -1,16 +1,16 @@
 export const onRequest: PagesFunction = async (ctx) => {
   const url = new URL(ctx.request.url);
 
-  // remove /f from path
   const path = url.pathname.replace(/^\/f/, "");
 
-  // Get ImageKit base URL from secret
-  const base = ctx.env.IK_BASE;  
-  const ikURL = base + path;
+  const base = ctx.env.IK_BASE; 
+  const ikURL = new URL(base + path);
 
-  const resp = await fetch(ikURL, {
-    cf: { cacheEverything: true },
+  url.searchParams.forEach((v, k) => {
+    ikURL.searchParams.set(k, v);
   });
 
-  return resp;
+  return fetch(ikURL.toString(), {
+    cf: { cacheEverything: true }
+  });
 };
